92df5e97bdb193e4853a1a9aec749a1e33273dfe
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-22 20:45:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add your JavaScript code here
+console.log("Welcome to iRunBook.ai");
+
+// Example interaction with the website's elements
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    header.addEventListener("click", function () {
+        alert("You clicked the header!");
+    });
+});
