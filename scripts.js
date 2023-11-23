c1ac1daa84b1b92f46bc9568204656b48d98ceff
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-23 01:48:52 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const kubectlCommandLink = 'https://raw.githubusercontent.com/kodesam/openshift_cli_in_juypter/main/kubectl%20get%20nodes.yaml';
+    const kubectlCommandElement = document.querySelector('a[href="' + kubectlCommandLink + '"]');
+
+    fetch(kubectlCommandLink)
+        .then(response => response.text())
+        .then(yamlContent => {
+            // Update the link with the YAML raw content as the href attribute
+            kubectlCommandElement.setAttribute('href', 'data:text/yaml;charset=utf-8,' + encodeURIComponent(yamlContent));
+        })
+        .catch(error => {
+            console.error('Error loading YAML content: ' + error);
+        });
+});
