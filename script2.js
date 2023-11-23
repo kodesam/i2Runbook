ec89c1b6c9046a90bba3590d70704d9617a4fd7c
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-23 01:55:36 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(yamlContent => {
             // Update the link with the YAML raw content as the href attribute
-            kubectlCommandElement.setAttribute('href', 'data:text/yaml;charset=utf-8,' + encodeURIComponent(yamlContent));
+            kubectlCommandElement.setAttribute('href', 'data:yaml;charset=utf-8,' + encodeURIComponent(yamlContent));
         })
         .catch(error => {
             console.error('Error loading YAML content: ' + error);
